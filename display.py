e60e05db229d59dba27d8de0a5fa4982e1590754
Florent Sarembaud <cromod@users.noreply.github.com>
2016-06-14 01:30:17 +0200

    Improve display module

--- display.py
+++ display.py
@@ -1,7 +1,5 @@
 import Tkinter as tk
 import solver
-import time
-import sys
 
 def _create_circle(self, x, y, r, **kwargs):
     return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
@@ -11,36 +9,31 @@ def _coords_circle(self, target, x, y, r, **kwargs):
     return self.coords(target, x-r, y-r, x+r, y+r, **kwargs)
 tk.Canvas.coords_circle = _coords_circle
 
-def draw(ball_list,canvas):
-    draw_list = []
-    for ball in ball_list:
-        position = ball.position.tolist()
-        draw_list.append(canvas.create_circle(position[0], position[1], ball.radius, fill="white"))
-    return draw_list
+def display(balls, canvas):
+    return [canvas.create_circle(ball.position[0], ball.position[1], ball.radius, fill="white") for ball in balls]
 
-def move(ball_list,draw_list,canvas):
-    for i in range(len(ball_list)):
-        position = ball_list[i].position.tolist()
-        canvas.coords_circle(draw_list[i],position[0], position[1],ball_list[i].radius)
+def move(balls, drawing, canvas):
+    for i, ball in enumerate(balls):
+        canvas.coords_circle(drawing[i], ball.position[0], ball.position[1], ball.radius)
 
 def stop(event):
     canvas.destroy()
 
-def animation(ball_list, draw_list, canvas, step, wlength):
+def animation(balls, drawing, canvas, step, wlength):
     flag = True
     while(flag):
-        solver.solve_step(ball_list,step,wlength)
-        move(ball_list,draw_list,canvas)
+        solver.solve_step(balls, step, wlength)
+        move(balls, drawing, canvas)
         canvas.update()
         #time.sleep(step)
         #canvas.bind('<KeyPress>', stop)
 
-def run(ball_list, step, wlength):
+def run(balls, step, wlength):
     window = tk.Tk()
     canvas = tk.Canvas(window, width=wlength, height=wlength, bg="black")
     canvas.pack()
-    draw_list = draw(ball_list,canvas)
+    drawing = display(balls, canvas)
     canvas.pack()
     canvas.focus_set()
-    window.after(0,animation(ball_list, draw_list, canvas, step, wlength))
+    window.after(0, animation(balls, drawing, canvas, step, wlength))
     window.mainloop()
